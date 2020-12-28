c289dcfd9e155c854fe85d5c0ffda3c4b2754ae1
frankb2021 <76616249+frankb2021@users.noreply.github.com>
2020-12-28 15:34:59 +0100

    test
    test

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let addButton = document.querySelector('.add-todo');
 let emptyList = document.querySelector('.container1')
 
-addButton.addEventListener('click', function(){
+addButton.addEventListener('click', function() {
 
     let newtask = document.createElement('li');
     let node = document.createTextNode('vuur');
     newtask.appendChild(node);
-    document.querySelector('.container1').appendChild(newtask);
+    document.querySelector('.ul-list').appendChild(newtask);
 })
