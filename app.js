0ec7765efb88858f1a0854a0a218bafea5544b34
frankb2021 <76616249+frankb2021@users.noreply.github.com>
2020-12-28 16:03:43 +0100

    work on ad function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let addButton = document.querySelector('.add-todo');
 let emptyList = document.querySelector('.container1')
 
-addButton.addEventListener('click', function() {
+addButton.addEventListener('click', function(event) {
+
+    event.preventDefault();
 
     let newtask = document.createElement('li');
     let node = document.createTextNode('vuur');
