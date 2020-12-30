9ad353413075ec9e494761c590f707f30834bb7a
frankb2021 <76616249+frankb2021@users.noreply.github.com>
2020-12-30 16:09:11 +0100

    add function works

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+
 let addButton = document.querySelector('.add-todo');
-let emptyList = document.querySelector('.container1')
+let unlist = document.querySelector('.ul-list');
+let inputUser = document.querySelector('.input-task');
 
-addButton.addEventListener('click', function(event) {
-
+addButton.addEventListener('click',function(event){
     event.preventDefault();
+    
 
-    let newtask = document.createElement('li');
-    let node = document.createTextNode('vuur');
-    newtask.appendChild(node);
-    document.querySelector('.ul-list').appendChild(newtask);
+    let newLi = document.createElement('li');
+    let node = document.querySelector('.input-task').value;
+    
+    newLi.innerHTML = node;
+    unlist.appendChild(newLi);
+    newLi.classList.add('newItemsMarkup')
 })
