fcbe4408336d6bd98abd366309662bb6837ba792
frankb2021 <76616249+frankb2021@users.noreply.github.com>
2021-01-01 22:58:16 +0100

    delete works now

--- app.js
+++ app.js
@@ -8,19 +8,41 @@ const todoList = document.querySelector('.to-do-list');
 // event
 
 
+todoList.addEventListener('click', deletecheck);
 todoButton.addEventListener('click', function(event){
 event.preventDefault();
 
 let newdiv  = document.createElement('div'); // creatre new div
 let deleteButton = document.createElement('button'); // create new button
+deleteButton.textContent  ="delete";
+deleteButton.classList  ="deleteitem";
 let inputval = todoInput.value;
 newdiv.innerHTML = inputval;
 newdiv.className = "newTodo"; // give div class name newdiv
 todoList.appendChild(newdiv); // append new div to class to-do-list
-todoList.appendChild(deleteButton); // append new button to class to-do-list
+newdiv.appendChild(deleteButton); // append new button to class to-do-list
 
 if(todoInput){
     todoInput.value = "";
 }
 
 })
+
+ function deletecheck(e){
+    const item = e.target;
+    
+if(item.classList[0] === "deleteitem"){
+    const todo = item.parentElement;
+    todo.remove();
+
+ 
+
+ }
+}
+
+    
+ 
+
+
+
+
